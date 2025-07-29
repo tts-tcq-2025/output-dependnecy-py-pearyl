cfaff259a8af6d020a4334bcda0193de6ec31cf0
pearyl <131762349+pearyl@users.noreply.github.com>
2025-07-29 11:57:04 +0530

    Update misaligned.test.py

--- misaligned.test.py
+++ misaligned.test.py
@@ -1,10 +1,26 @@
 import unittest
-from misaligned import print_color_map
+from misaligned import get_color_map, print_color_map
 
+def fake_print(line):
+    captured_lines.append(line)
 
 class MisalignedTest(unittest.TestCase):
-    def test_color_map(self):
-        pass
+    generated_manual = []
+    
+    def fake_print_fn(line):
+        generated_manual.append(line)
+    
+    # Call with fake print function
+    count = print_color_map(fake_print_fn)
+    
+    # Prepare actual expected output
+    expected_manual = [
+        f'{index} | {major} | {minor}'
+        for index, major, minor in get_color_map()
+    ]
+    
+    self.assertEqual(count, len(expected_manual))
+    self.assertEqual(generated_manual, expected_manual)
 
 if __name__ == '__main__':
   unittest.main()

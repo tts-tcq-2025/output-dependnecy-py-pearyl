424986339795c396b535ddb8806323960731061e
pearyl <131762349+pearyl@users.noreply.github.com>
2025-07-29 10:50:18 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,32 @@
+def get_major_colors():
+    return ["White", "Red", "Black", "Yellow", "Violet"]
 
-def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+def get_minor_colors():
+    return ["Blue", "Orange", "Green", "Brown", "Slate"]
+
+def get_color_map():
+    color_map = []
+    major_colors = get_major_colors()
+    minor_colors = get_minor_colors()
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            index = i * len(minor_colors) + j
+            color_map.append((index, major, minor))
+    return color_map
+
+def format_color_map_line(entry):
+    return (f'{index} | {major} | {minor}')
+
+def print_on_console(line_content: str):
+    print(line_content, end='')
+
+def print_color_map(print_fn: Callable[[str], None]) -> int:
+    color_map = generate_color_map()
+    for entry in color_map:
+        line = format_color_map_line(entry)
+        print_fn(line)
+    return len(color_map)
 
 
-result = print_color_map()
-assert(result == 25)
-print("All is well (maybe!)")
+
+
